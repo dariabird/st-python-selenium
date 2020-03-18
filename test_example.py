9cd7fd674d8701abe48a3f37ce19b23c856a0ba4
Daria Lopteva <daria.lopteva@abbyy.com>
2020-03-18 20:39:58 +0300

    Added litecart admin login scenario

--- test_example.py
+++ test_example.py
@@ -11,3 +11,13 @@ def driver(request):
 
 def test_example(driver):
     driver.get("http://www.google.com/")
+
+
+def test_login_litecart_admin(driver):
+    driver.get("http://localhost/litecart/admin/")
+    username_field = driver.find_element_by_name("username")
+    username_field.send_keys("admin")
+    password_field = driver.find_element_by_name("password")
+    password_field.send_keys("admin")
+    login_button = driver.find_element_by_name("login")
+    login_button.click()
